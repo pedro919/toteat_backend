8d8c5ea3edf868878fc7ec14922abda287f038a4
pirioja <pirioja@uc.cl>
2022-08-02 11:18:30 -0400

    feat: Added endpoints for monthly sales and employees sales

--- toteat_api/api.py
+++ toteat_api/api.py
@@ -1,8 +1,78 @@
 from ninja import NinjaAPI
+import json
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from .api_functions import months_betweeen_to_dates, analyse_monthly_sales, analyze_waiter_sales, analyze_cashier_sales
 
 api = NinjaAPI()
 
+f = open('toteat_api/data/ventas.json')
+data = json.load(f)
+
+for element in data:
+    element['date_closed'] = datetime.strptime(element['date_closed'], '%Y-%m-%d %H:%M:%S')
+    element['date_opened'] = datetime.strptime(element['date_opened'], '%Y-%m-%d %H:%M:%S')
+
+sorted_data = sorted(data, key=lambda x: x['date_closed'])
 
 @api.get("/hello")
 def hello(request):
     return "Hello world"
+
+
+@api.get("/monthly_sales")
+def calculate_monthly_sales(request):
+    response = []
+    start_date = sorted_data[0]['date_closed'].date()
+    end_date = sorted_data[-1]['date_closed'].date()
+    number_of_months = months_betweeen_to_dates(start_date, end_date)
+    actual_date = start_date.replace(day=1)
+    for i in range(number_of_months):
+        monthly_sales = list(filter(lambda x: (x['date_closed'].month == actual_date.month) and (x['date_closed'].year == actual_date.year), sorted_data))
+        monthly_sales_response = analyse_monthly_sales(actual_date, monthly_sales)
+        response.append(monthly_sales_response)
+        actual_date = actual_date + relativedelta(months=1)
+
+    return response
+
+
+@api.get("/total_sales_by_waiter")
+def calculate_sales_by_waiter(request):
+    response = analyze_waiter_sales(sorted_data)
+    return response
+
+
+@api.get("/monthly_sales_by_waiter")
+def calculate_monthly_sales_by_waiter(request):
+    response = []
+    start_date = sorted_data[0]['date_closed'].date()
+    end_date = sorted_data[-1]['date_closed'].date()
+    number_of_months = months_betweeen_to_dates(start_date, end_date)
+    actual_date = start_date.replace(day=1)
+    for i in range(number_of_months):
+        monthly_sales = list(filter(lambda x: (x['date_closed'].month == actual_date.month) and (x['date_closed'].year == actual_date.year), sorted_data))
+        monthly_sales_response = analyze_waiter_sales(monthly_sales)
+        response.append({'date': f'{actual_date.strftime("%m-%Y")}', 'waiters_sales': monthly_sales_response})
+
+    return response
+
+
+@api.get("/total_sales_by_cashier")
+def calculate_sales_by_cashier(request):
+    response = analyze_cashier_sales(sorted_data)
+    return response
+
+
+@api.get("/monthly_sales_by_cashier")
+def calculate_monthly_sales_by_cashier(request):
+    response = []
+    start_date = sorted_data[0]['date_closed'].date()
+    end_date = sorted_data[-1]['date_closed'].date()
+    number_of_months = months_betweeen_to_dates(start_date, end_date)
+    actual_date = start_date.replace(day=1)
+    for i in range(number_of_months):
+        monthly_sales = list(filter(lambda x: (x['date_closed'].month == actual_date.month) and (x['date_closed'].year == actual_date.year), sorted_data))
+        monthly_sales_response = analyze_cashier_sales(monthly_sales)
+        response.append({'date': f'{actual_date.strftime("%m-%Y")}', 'cashiers_sales': monthly_sales_response})
+
+    return response
---
+++ toteat_api/api_functions.py
@@ -0,0 +1,132 @@
+from datetime import datetime, timedelta
+
+def last_day_of_month(any_day):
+    next_month = any_day.replace(day=28) + timedelta(days=4)
+    return next_month - timedelta(days=next_month.day)
+
+def months_betweeen_to_dates(from_date, to_date):
+    if from_date.year == to_date.year:
+        return to_date.month - from_date.month + 1
+    else:
+        return 12*(to_date.year - from_date.year) - to_date.month + from_date.month + 1
+
+def analyse_monthly_sales(date, monthly_sales):
+    response_json = {'date': f'{date.strftime("%m-%Y")}'}
+    zones_count = {}
+    zones_income = {}
+    payments_count = {}
+    payments_income = {}
+    categories_count = {}
+    categories_income = {}
+    products_by_category_count = {}
+    products_by_category_income = {}
+    total_income = 0
+    total_clients = 0
+    total_time = 0
+
+    for sale in monthly_sales:
+        if sale['zone'] in zones_count:
+            zones_count[sale['zone']] += 1
+            zones_income[sale['zone']] += sale['total']
+        else:
+            zones_count[sale['zone']] = 1
+            zones_income[sale['zone']] = sale['total']
+        
+        for payment in sale['payments']:
+            if payment['type'] in payments_count:
+                payments_count[payment['type']] += 1
+                payments_income[payment['type']] += payment['amount']
+            else:
+                payments_count[payment['type']] = 1
+                payments_income[payment['type']] = payment['amount']
+        
+        for product in sale['products']:
+            total_product_income =  int(product['price']) * int(product['quantity'])
+            if product['category'] in categories_count:
+                categories_count[product['category']] += int(product['quantity'])                
+                categories_income[product['category']] += total_product_income
+                
+                if product['name'] in products_by_category_count[product['category']]:
+                    products_by_category_count[product['category']][product['name']] += int(product['quantity'])                    
+                    products_by_category_income[product['category']][product['name']] += total_product_income
+                else:
+                    products_by_category_count[product['category']][product['name']] = int(product['quantity'])
+                    products_by_category_income[product['category']][product['name']] = total_product_income
+
+            else:
+                categories_count[product['category']] = int(product['quantity'])
+                categories_income[product['category']] = int(product['price']) * int(product['quantity'])
+                products_by_category_count[product['category']] = {product['name']: int(product['quantity'])}
+                products_by_category_income[product['category']] = {product['name']: total_product_income}
+        
+        total_income += sale['total']
+        total_clients += sale['diners']
+        total_time += (sale['date_closed'] - sale['date_opened']).total_seconds() / 60
+        
+    response_json['zones_count'] = zones_count
+    response_json['zones_income'] = zones_income
+    response_json['total_income'] = total_income
+    response_json['total_clients'] = total_clients
+    response_json['income_per_client'] = total_income/total_clients
+    response_json['average_time_in_restaurant'] = total_time / len(monthly_sales)
+    response_json['payments_count'] = payments_count
+    response_json['payments_income'] = payments_income
+    response_json['categories_count'] = categories_count
+    response_json['categories_income'] = categories_income
+    response_json['products_count'] = products_by_category_count
+    response_json['product_income'] = products_by_category_income
+
+    return response_json
+
+
+def analyze_waiter_sales(sales):
+    waiter_sales_count = {}
+    waiter_sales_income = {}
+    waiter_attended_clientes = {}
+    response_json = []
+    for sale in sales:
+        if sale['waiter'] in waiter_sales_count:
+            waiter_sales_count[sale['waiter']] += 1
+            waiter_sales_income[sale['waiter']] += sale['total']
+            waiter_attended_clientes[sale['waiter']] += sale['diners']
+        else:
+            waiter_sales_count[sale['waiter']] = 1
+            waiter_sales_income[sale['waiter']] = sale['total']
+            waiter_attended_clientes[sale['waiter']] = sale['diners']
+    
+    for person in waiter_sales_count:
+        person_dictionary = {}
+        person_dictionary['name'] = person
+        person_dictionary['total_sales_count'] = waiter_sales_count[person]
+        person_dictionary['total_sales_income'] = waiter_sales_income[person]
+        person_dictionary['total_earned_tip'] = int(waiter_sales_income[person]) * 0.1
+        person_dictionary['total_attended_clients'] = waiter_attended_clientes[person]
+        response_json.append(person_dictionary)
+
+    return response_json
+
+
+def analyze_cashier_sales(sales):
+    cashier_sales_count = {}
+    cashier_sales_income = {}
+    cashier_attended_clientes = {}
+    response_json = []
+    for sale in sales:
+        if sale['cashier'] in cashier_sales_count:
+            cashier_sales_count[sale['cashier']] += 1
+            cashier_sales_income[sale['cashier']] += sale['total']
+            cashier_attended_clientes[sale['cashier']] += sale['diners']
+        else:
+            cashier_sales_count[sale['cashier']] = 1
+            cashier_sales_income[sale['cashier']] = sale['total']
+            cashier_attended_clientes[sale['cashier']] = sale['diners']
+    
+    for person in cashier_sales_count:
+        person_dictionary = {}
+        person_dictionary['name'] = person
+        person_dictionary['total_sales_count'] = cashier_sales_count[person]
+        person_dictionary['total_sales_income'] = cashier_sales_income[person]
+        person_dictionary['total_attended_clients'] = cashier_attended_clientes[person]
+        response_json.append(person_dictionary)
+
+    return response_json
